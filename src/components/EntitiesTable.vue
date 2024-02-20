233bbde23ac070740250f54bcac95cdb55879f31
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-19 20:32:30 -0500

    Make basic entities table

---
+++ src/components/EntitiesTable.vue
@@ -0,0 +1,19 @@
+<template>
+  <VDataTableServer :headers="headers"></VDataTableServer>
+</template>
+
+<script setup>
+const headers = [
+  {
+    title: "Nombre",
+    key: "name"
+  },
+  { title: "Pico (kWh)", key: "pike" },
+  { title: "Día (kWh)", key: "day" },
+  { title: "Madrugada (kWh)", key: "morning" },
+  { title: "Reactivo (kVArh)", key: "reactive" },
+  { title: "MDP (kW)", key: "mdp" },
+  { title: "MDD (kW)", key: "mdd" },
+  { title: "MDM (kW)", key: "mdm" }
+];
+</script>
