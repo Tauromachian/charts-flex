5bbe95541864e069340d13c0a7f8d68d1ead9aee
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-19 20:07:37 -0500

    Add Vuetify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    Vuetify()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    },
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"]
+  }
 });
