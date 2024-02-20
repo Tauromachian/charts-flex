a8c7266c414a8eff94c0b75dc9bd5202eeb71daf
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-19 20:32:20 -0500

    Make basic HomePage

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,60 @@
+<template>
+  <VApp>
+    <VContainer>
+      <VRow>
+        <VCol cols="12" md="2"
+          ><VCard
+            ><VCardTitle>Some Title</VCardTitle><VCardText></VCardText
+          ></VCard>
+        </VCol>
+        <VCol cols="12" md="4"
+          ><VCard
+            ><VCardTitle>Some Title</VCardTitle><VCardText></VCardText
+          ></VCard>
+        </VCol>
+        <VCol cols="12" md="1"
+          ><VCard
+            ><VCardTitle>Some Title</VCardTitle><VCardText></VCardText
+          ></VCard>
+        </VCol>
+        <VCol cols="12" md="5"
+          ><VCard
+            ><VCardTitle>Some Title</VCardTitle><VCardText></VCardText
+          ></VCard>
+        </VCol>
+        <VCol cols="12" md="1"
+          ><VCard
+            ><VCardTitle>Some Title</VCardTitle><VCardText></VCardText
+          ></VCard>
+        </VCol>
+        <VCol cols="12" md="5"
+          ><VCard
+            ><VCardTitle>Some Title</VCardTitle><VCardText></VCardText
+          ></VCard>
+        </VCol>
+        <VCol cols="12" md="1"
+          ><VCard
+            ><VCardTitle>Some Title</VCardTitle><VCardText></VCardText
+          ></VCard>
+        </VCol>
+        <VCol cols="12" md="5"
+          ><VCard
+            ><VCardTitle>Some Title</VCardTitle><VCardText></VCardText
+          ></VCard>
+        </VCol>
+        <VCol cols="12" md="7">
+          <VCard>
+            <VCardTitle>Entidades</VCardTitle
+            ><VCardText>
+              <EntitiesTable></EntitiesTable>
+            </VCardText>
+          </VCard>
+        </VCol>
+      </VRow>
+    </VContainer>
+  </VApp>
+</template>
+
+<script setup>
+import EntitiesTable from "@/components/EntitiesTable.vue";
+</script>
