7fa0f197136153b94b0831de1f9a665ce3fe05dd
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-24 10:29:46 -0500

    Make basic Chart card

---
+++ src/components/ChartCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card class="d-flex flex-column">
+    <v-card-text>
+      <h1>{{ title }}</h1>
+    </v-card-text>
+    <v-card-text class="py-1">
+      <div class="d-flex flex-wrap gap-3">
+        <span
+          v-for="(legendValue, index) in legendValues"
+          :key="`legend-badge-${index}`"
+        >
+          <v-badge inline :color="legendValue.color" dot></v-badge>
+          <span>{{ legendValue.label }}</span>
+        </span>
+      </div>
+    </v-card-text>
+    <v-card-text class="flex-grow-1">
+      <div class="relative h-100">
+        <slot></slot>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: ""
+  },
+  legendValues: {
+    type: Array,
+    default: () => []
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
