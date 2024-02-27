e1e58dab4515d5e85752fea9435a117ec83c8b52
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-26 20:42:16 -0500

    Set theme colors on Vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,7 +5,17 @@ import "vuetify/styles";
 // Composables
 import { createVuetify } from "vuetify";
 
+const themeColors = {
+  dark: false,
+  colors: { accent: "#703D57", neutral: "#ebfdff" }
+};
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  //
+  theme: {
+    defaultTheme: "themeColors",
+    themes: {
+      themeColors
+    }
+  }
 });
