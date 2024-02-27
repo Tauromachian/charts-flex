f7439dc5d5c4ae50b5b1bf316493916f40488a41
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-26 20:29:07 -0500

    Add chart general options

---
+++ src/utils/chartGeneralOptions.js
@@ -0,0 +1,41 @@
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    y: {
+      ticks: {
+        color: "#b6baca" // Change the font color of the numbers
+      },
+      grid: {
+        drawTicks: false
+      },
+      border: {
+        dash: [5, 10]
+      }
+    },
+    x: {
+      ticks: {
+        color: "#b6baca"
+      },
+      grid: {
+        display: false,
+        drawOnChartArea: false
+      },
+      border: {
+        display: false
+      }
+    }
+  },
+  plugins: {
+    legend: {
+      display: false
+    },
+    tooltip: {
+      backgroundColor: "#240115"
+    }
+  },
+  interaction: {
+    intersect: false,
+    mode: "index"
+  }
+};
