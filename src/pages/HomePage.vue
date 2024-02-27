226c74b898750d0091039e32adfa757450794b8c
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2024-02-26 20:17:43 -0500

    Move line chart down

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,13 +2,9 @@
   <VApp>
     <VContainer>
       <VRow>
-        <VCol cols="12" md="2">
+        <VCol cols="12" md="2"> </VCol>
+        <VCol cols="12" md="4">
           <ChartService></ChartService>
-        </VCol>
-        <VCol cols="12" md="4"
-          ><VCard
-            ><VCardTitle>Some Title</VCardTitle><VCardText></VCardText
-          ></VCard>
         </VCol>
         <VCol cols="12" md="1"
           ><VCard
@@ -55,4 +51,5 @@
 
 <script setup>
 import EntitiesTable from "@/components/EntitiesTable.vue";
+import ChartService from "@/components/ChartService.vue";
 </script>
